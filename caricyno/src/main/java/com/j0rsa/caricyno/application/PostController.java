f241843177950121ca8484ba2fe87e460b871b83
keyridan <keyridan@gmail.com>
2017-05-26 14:26:05 +0200

    added create and save post, converter for NewsObject changed to convert from Post instead of WallFullPost

--- caricyno/src/main/java/com/j0rsa/caricyno/application/PostController.java
+++ caricyno/src/main/java/com/j0rsa/caricyno/application/PostController.java
@@ -1,6 +1,8 @@
 package com.j0rsa.caricyno.application;
 
 import com.j0rsa.caricyno.vk.NewsService;
+import com.j0rsa.caricyno.website.producer.NewsObject;
+import com.j0rsa.caricyno.website.producer.NewsPublisher;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.wall.WallpostFull;
@@ -12,17 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
 public class PostController {
     private final NewsService newsService;
     private final ConversionService conversionService;
+    private final NewsPublisher newsPublisher;
 
     @Autowired
-    public PostController(NewsService newsService, ConversionService conversionService) {
+    public PostController(NewsService newsService, ConversionService conversionService, NewsPublisher newsPublisher) {
         this.newsService = newsService;
         this.conversionService = conversionService;
+        this.newsPublisher = newsPublisher;
     }
 
     GetResponse getNews() throws ClientException, ApiException {
@@ -38,5 +46,15 @@ public class PostController {
             posts.add(conversionService.convert(wallpostFull, Post.class));
         }
         return posts;
+    }
+
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    public NewsObject create(Post post) {
+        return conversionService.convert(post, NewsObject.class);
+    }
+
+    @RequestMapping(value = "posts/save", method = RequestMethod.POST)
+    public void save(NewsObject newsObject) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        newsPublisher.publish(newsObject);
     }
 }
--- caricyno/src/main/java/com/j0rsa/caricyno/application/converters/ToNewsObjectConverter.java
+++ caricyno/src/main/java/com/j0rsa/caricyno/application/converters/ToNewsObjectConverter.java
@@ -1,7 +1,9 @@
 package com.j0rsa.caricyno.application.converters;
 
+import com.j0rsa.caricyno.application.Post;
 import com.j0rsa.caricyno.website.producer.NewsObject;
-import com.vk.api.sdk.objects.wall.WallpostFull;
+import com.j0rsa.caricyno.website.producer.enums.ArticleRights;
+import com.j0rsa.caricyno.website.producer.enums.Category;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -9,18 +11,24 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-public class ToNewsObjectConverter implements Converter<WallpostFull, NewsObject> {
+public class ToNewsObjectConverter implements Converter<Post, NewsObject> {
     private static List<String> POST_TAGS = Arrays.asList("царицыно", "новости", "жкцарицыно", "достройтецарицыно", "дольщики");
     private static String TEMPLATE = "<p><span style=\"font-size: 12pt; font-family: 'times new roman', times;\">%s</span></p>";
+    private static Category DEFAULT_CATEGORY = Category.OTHER;
+    private static ArticleRights DEFAULT_ARTICLES_RIGHTS = ArticleRights.EVERYONE;
+    private static ArticleRights DEFAULT_COMMENT_RIGHTS = ArticleRights.EVERYONE;
 
     @Override
-    public NewsObject convert(WallpostFull wallpostFull) {
+    public NewsObject convert(Post post) {
         NewsObject newsObject = new NewsObject();
-        newsObject.setTitle(wallpostFull.getText());
+        newsObject.setTitle(post.getText());
         newsObject.setTags(POST_TAGS);
 
-        String text = String.format(TEMPLATE, wallpostFull.getText());
+        String text = String.format(TEMPLATE, post.getText());
         newsObject.setHtmlText(text);
+        newsObject.setCategory(DEFAULT_CATEGORY);
+        newsObject.setVisibility(DEFAULT_ARTICLES_RIGHTS);
+        newsObject.setCommentsRights(DEFAULT_COMMENT_RIGHTS);
 
         return newsObject;
     }
