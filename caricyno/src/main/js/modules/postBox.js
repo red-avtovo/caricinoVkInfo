e73a7e83f783885759d47121c23faa0d250d9c25
Alexandr Bochev <red.avtovo@gmail.com>
2017-05-27 16:24:57 +0200

    cleaned web project added modal windows refactored cards with thumbnails

--- caricyno/src/main/js/modules/postBox.js
+++ caricyno/src/main/js/modules/postBox.js
@@ -1,22 +1,98 @@
 'use strict';
 
 const React = require('react');
+const {Modal, Button, Col, Thumbnail, Label, Well, Collapse, ButtonToolbar} = require('react-bootstrap');
 
 class PostBox extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {post: props.post, modal: false, fullText: false};
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.toggleFullText = this.toggleFullText.bind(this);
+    }
+
+    openModal() {
+        this.setState({modal: true})
+    }
+
+    closeModal() {
+        this.setState({modal: false})
+    }
+
+    toggleFullText() {
+        this.setState({fullText: !this.state.fullText})
+    }
 
     render() {
-        const post = this.props.post;
         return (
-            <div className='card'>
-                <div className='card-block'>
-                    <h4 className='card-title'>{post.title}</h4>
-                    <span className='badge badge-default badge-pill badge-info'>{post.author}</span>
-                    <p className='card-text'>
-                        {post.text}
-                        <a href='#' className='btn btn-primary btn-sm'>Create news post</a>
-                    </p>
-                </div>
+            <div>
+                <Col xs={6} md={4}>
+                    <Thumbnail className="postCard">
+                        <ButtonToolbar>
+                            <Button bsStyle="danger" bsSize="small" className="pull-right">Ignore</Button>
+                            <Button bsStyle="primary" bsSize="small" className="pull-right" onClick={this.openModal}>Create
+                                news
+                                post</Button>&nbsp;
+                        </ButtonToolbar>
+                        <div className="titleContainer">
+                            <div className="ellipsis">
+                                {this.state.post.title}
+                            </div>
+                        </div>
+                        <p>
+                            <Label bsStyle="info">{this.state.post.author}</Label>
+                        </p>
+
+                        <Button onClick={this.toggleFullText}>
+                            Toggle full text
+                        </Button>
+                        <Collapse in={this.state.fullText}>
+                            <div dangerouslySetInnerHTML={{__html: this.state.post.text}}>
+                            </div>
+                        </Collapse>
+                    </Thumbnail>
+                </Col>
+
+                <Modal show={this.state.modal} onHide={this.closeModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>Create news post</h4>
+                        <hr />
+
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+
+                        <h4>Overflowing text to show scroll behavior</h4>
+                        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                            egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus
+                            vel augue laoreet rutrum faucibus dolor auctor.</p>
+                        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque
+                            nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor
+                            fringilla.</p>
+                        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                            egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus
+                            vel augue laoreet rutrum faucibus dolor auctor.</p>
+                        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque
+                            nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor
+                            fringilla.</p>
+                        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                            egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus
+                            vel augue laoreet rutrum faucibus dolor auctor.</p>
+                        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque
+                            nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor
+                            fringilla.</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.closeModal}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }
--- caricyno/webpack.config.js
+++ caricyno/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     debug: true,
     output: {
         path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        // filename: './src/main/resources/static/built/bundle.js'
+        filename: './target/classes/static/built/bundle.js'
     },
     module: {
         loaders: [
