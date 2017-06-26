101230fd8d928e45b17695a8b4cb1d9abfb34635
Alexandr Bochev <red.avtovo@gmail.com>
2017-06-26 23:48:27 +0200

    added custom link wrappers

--- caricyno/src/main/java/com/j0rsa/caricyno/application/Utils.java
+++ caricyno/src/main/java/com/j0rsa/caricyno/application/Utils.java
@@ -1,5 +1,6 @@
 package com.j0rsa.caricyno.application;
 
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,17 +16,50 @@ public class Utils {
         return o == null;
     }
 
+    private static Function<String, Boolean> linkIsImage = link -> {
+        final String trimmedLink = link.trim();
+        return trimmedLink.endsWith("jpg") ||
+                trimmedLink.endsWith("jpeg") ||
+                trimmedLink.endsWith("png") ||
+                trimmedLink.endsWith("gif") ||
+                trimmedLink.endsWith("bmp");
+    };
+    private static Function<String, Boolean> linkIsYoutubeLink = link -> {
+        final String trimmedLink = link.trim();
+        return trimmedLink.startsWith("https://www.youtube.com/watch?v=");
+    };
+
     public static String wrapLinks(String htmlString) {
         Matcher matcher = pattern.matcher(htmlString);
         StringBuffer sb = new StringBuffer(htmlString.length());
         while (matcher.find()) {
             String text = matcher.group(1);
-            matcher.appendReplacement(sb,
-                    String.format("<a href='%1$s'>%1$s</a>",
-                            Matcher.quoteReplacement(text))
-            );
+            matcher.appendReplacement(sb, wrapLink(Matcher.quoteReplacement(text)));
         }
         matcher.appendTail(sb);
         return sb.toString();
+    }
+
+    private static String wrapLink(final String text) {
+        final String lowerCasedText = text.toLowerCase().trim();
+        if (linkIsImage.apply(lowerCasedText)) {
+            return String.format("<img src='%s' alt=''>", text);
+        }
+        if (linkIsYoutubeLink.apply(lowerCasedText)) {
+            return wrapYoutubeLink(text);
+        }
+        return String.format("<a href='%1$s'>%1$s</a>", text);
+
+    }
+
+    private static String wrapYoutubeLink(String text) {
+        String trimmedText = text.trim();
+        String videoId = trimmedText.substring("https://www.youtube.com/watch?v=".length());
+        return String.format(
+                "<iframe width=\"560\" height=\"315\" " +
+                        "src=\"https://www.youtube.com/embed/%s\" " +
+                        "frameborder=\"0\" " +
+                        "allowfullscreen>" +
+                        "</iframe>", videoId);
     }
 }
