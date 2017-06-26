23704d8cda5b393047553459d9feb7bc806dfab2
Alexandr Bochev <red.avtovo@gmail.com>
2017-06-26 23:16:17 +0200

    rewrote link wrapping with pattern matcher

--- caricyno/src/main/java/com/j0rsa/caricyno/application/Utils.java
+++ caricyno/src/main/java/com/j0rsa/caricyno/application/Utils.java
@@ -1,7 +1,11 @@
 package com.j0rsa.caricyno.application;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utils {
     public static final String IMAGE_TEMPLATE = "<img src='$1' alt=''>";
+    private static final Pattern pattern = Pattern.compile("([http|ftp|https]+://[^<> ]+)");
 
     public static boolean isNotNull(Object o) {
         return !isNull(o);
@@ -11,8 +15,17 @@ public class Utils {
         return o == null;
     }
 
-    public static String wrapLinks(String text) {
-        return text
-                .replaceAll("([http|ftp|https]+://[^<> ]+)", "<a href='$1'>$1</a>");
+    public static String wrapLinks(String htmlString) {
+        Matcher matcher = pattern.matcher(htmlString);
+        StringBuffer sb = new StringBuffer(htmlString.length());
+        while (matcher.find()) {
+            String text = matcher.group(1);
+            matcher.appendReplacement(sb,
+                    String.format("<a href='%1$s'>%1$s</a>",
+                            Matcher.quoteReplacement(text))
+            );
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 }
