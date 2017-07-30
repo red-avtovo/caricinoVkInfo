9855c51b5bcd6507ea445742269d706595a3fef0
Alexandr Bochev <red.avtovo@gmail.com>
2017-07-30 20:10:14 +0200

    disable csrf

--- caricyno/src/main/java/com/j0rsa/caricyno/application/WebSecurityConfig.java
+++ caricyno/src/main/java/com/j0rsa/caricyno/application/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.j0rsa.caricyno.application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,6 +15,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public WebSecurityConfig(ApplicationProperties applicationProperties) {
         this.applicationProperties = applicationProperties;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 
     @Autowired
